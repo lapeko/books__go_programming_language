181de7cbad14bfbf1f3a574cd77752694cc760c4
lapeko <lapeko88@gmail.com>
2024-08-23 16:20:05 +0200

    modify app to show filenames with same lines

--- chapter-1/exercise-2/main.go
+++ chapter-1/exercise-2/main.go
@@ -1,3 +1,4 @@
+// Finds identical lines across multiple files and lists the files where each line occurs.
 package main
 
 import (
@@ -7,19 +8,28 @@ import (
 )
 
 func main() {
-	counts := map[string]int{}
+	counts := map[string]map[string]bool{}
 	args := os.Args[1:]
 
 	if len(args) < 1 {
-		countLines(os.Stdin, counts)
+		fmt.Println("No files provided in args. Exit process...")
+		return
 	} else {
 		readLinesFromFiles(args, counts)
 	}
 
-	fmt.Printf("%v", counts)
+	for key, set := range counts {
+		if len(set) > 1 {
+			fmt.Print("Key: ", key, " is met in files: ")
+			for key, _ := range set {
+				fmt.Print(key, " ")
+			}
+			fmt.Println()
+		}
+	}
 }
 
-func readLinesFromFiles(files []string, counts map[string]int) {
+func readLinesFromFiles(files []string, counts map[string]map[string]bool) {
 	for _, fileName := range files {
 		file, err := os.Open(fileName)
 
@@ -28,7 +38,17 @@ func readLinesFromFiles(files []string, counts map[string]int) {
 			continue
 		}
 
-		countLines(file, counts)
+		reader := bufio.NewScanner(file)
+		for reader.Scan() {
+			text := reader.Text()
+			if counts[text] == nil {
+				counts[text] = map[string]bool{}
+			}
+			counts[text][fileName] = true
+		}
+		if err := reader.Err(); err != nil {
+			fmt.Println("Read file error:", err)
+		}
 
 		err = file.Close()
 		if err != nil {
@@ -36,15 +56,4 @@ func readLinesFromFiles(files []string, counts map[string]int) {
 			return
 		}
 	}
-}
-
-func countLines(file *os.File, counts map[string]int) {
-	reader := bufio.NewScanner(file)
-	for reader.Scan() {
-		counts[reader.Text()]++
-	}
-	if err := reader.Err(); err != nil {
-		fmt.Println("Read file error:", err)
-	}
-
 }
