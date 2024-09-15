d70b621cfe30f724f3eb81ac9d5afcbcf3b6535a
lapeko <lapeko88@gmail.com>
2024-09-15 15:52:19 +0200

    chapter1 lissajous. use mope colors

--- chapter-1/exercise-3/main.go
+++ chapter-1/exercise-3/main.go
@@ -16,11 +16,22 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.RGBA{G: 0xFF, A: 0xFF}, color.Black}
+var palette = []color.Color{
+	color.RGBA{G: 0xFF, A: 0xFF},
+	color.White,
+	color.Black,
+	color.RGBA{R: 0xFF, A: 0xFF},
+	color.RGBA{B: 0xFF, A: 0xFF},
+	color.RGBA{R: 0xFF, B: 0xFF, A: 0xFF},
+}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	greenIndex = 0
+	whiteIndex = 1
+	blackIndex = 2
+	redIndex   = 3
+	blueIndex  = 4
+	pinkIndex  = 5
 )
 
 func main() {
@@ -47,15 +58,28 @@ func lissajous(out io.Writer) {
 	)
 	freq := rand.Float64() * 3.0
 	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
+	phase := 0.0
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			var colorIndex int
+			switch {
+			case t < 2*math.Pi:
+				colorIndex = whiteIndex
+			case t < 2*2*math.Pi:
+				colorIndex = blackIndex
+			case t < 3*2*math.Pi:
+				colorIndex = redIndex
+			case t < 4*2*math.Pi:
+				colorIndex = blueIndex
+			default:
+				colorIndex = pinkIndex
+			}
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				uint8(colorIndex))
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
