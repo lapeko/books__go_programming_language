3c5890181158e8a0f6eec2ae4c281b19db84a186
lapeko <lapeko88@gmail.com>
2025-01-16 15:18:09 +0100

    add counter and show headers and query params

--- chapter-1/exercise-5/main.go
+++ chapter-1/exercise-5/main.go
@@ -4,13 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+)
+
+var (
+	mut     sync.Mutex
+	counter = 0
 )
 
 func main() {
-	http.HandleFunc("/", handleFunc)
-	log.Fatalln(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/counter", handleCount)
+	mux.HandleFunc("/", handleMain)
+	log.Fatalln(http.ListenAndServe(":8080", mux))
 }
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Requested url is: %q", r.URL.Path)
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	mut.Lock()
+	counter++
+	mut.Unlock()
+	_, _ = fmt.Fprintf(w, "Requested url is: %q\n", r.URL.Path)
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+	}
+	for k, v := range r.Form {
+		_, _ = fmt.Fprintf(w, "Query[%q]=%q\n", k, v)
+	}
+	for k, v := range r.Header {
+		_, _ = fmt.Fprintf(w, "Header[%q]=%q\n", k, v)
+	}
+}
+
+func handleCount(w http.ResponseWriter, req *http.Request) {
+	mut.Lock()
+	count := counter
+	mut.Unlock()
+	_, _ = fmt.Fprintf(w, "counter: %d", count)
 }
