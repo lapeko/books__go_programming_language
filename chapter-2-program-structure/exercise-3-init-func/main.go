43d702f3720a160d96a921ba28a4cad2b4794565
lapeko <lapeko88@gmail.com>
2025-01-16 16:24:20 +0100

    count nonzero bytes in int64

---
+++ chapter-2-program-structure/exercise-3-init-func/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCountWithTable(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountInLoop(x uint64) int {
+	popCount := 0
+	for x > 0 {
+		popCount += int(x & 1)
+		x >>= 1
+	}
+	return popCount
+}
+
+func main() {
+	randomNum := rand.Uint64()
+
+	runTest(randomNum, PopCountWithTable)
+	runTest(randomNum, PopCountInLoop)
+}
+
+func runTest(randomNum uint64, fn func(x uint64) int) {
+	start := time.Now()
+	for i := 0; i < 10_000_000; i++ {
+		fn(randomNum)
+	}
+	res := fn(randomNum)
+	duration := time.Since(start)
+	fmt.Printf("randNum: %d, number of non zero bytes: %d, duration: %d nS\n", randomNum, res, duration.Nanoseconds())
+}
