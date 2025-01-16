89916ffdbd43c3d2552c93432f0ad3e40717962b
lapeko <lapeko88@gmail.com>
2025-01-16 09:44:24 +0100

    add http prefix if not exist

--- chapter-1/exercise-4/main.go
+++ chapter-1/exercise-4/main.go
@@ -6,10 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http") {
+			url = fmt.Sprintf("%s%s", "http://", url)
+		}
 		res, err := http.Get(url)
 		if err != nil {
 			log.Fatalln(fmt.Errorf("http get error: %w", err))
