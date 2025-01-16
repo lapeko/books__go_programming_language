01bf3e2b7fc90f466a4cc1f19028033378905da8
lapeko <lapeko88@gmail.com>
2025-01-16 10:10:45 +0100

    fetch urls concurrently

--- chapter-1/exercise-4/main.go
+++ chapter-1/exercise-4/main.go
@@ -2,29 +2,36 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	ch := make(chan int)
 	for _, url := range os.Args[1:] {
-		if !strings.HasPrefix(url, "http") {
-			url = fmt.Sprintf("%s%s", "http://", url)
-		}
-		res, err := http.Get(url)
-		if err != nil {
-			log.Fatalln(fmt.Errorf("http get error: %w", err))
-		}
-		fmt.Printf("Status code: %d\n", res.StatusCode)
-		_, err = io.Copy(os.Stdout, res.Body)
-		if err := res.Body.Close(); err != nil {
-			log.Println(fmt.Errorf("http body close error: %w", err))
-		}
-		if err != nil {
-			log.Fatalln(fmt.Errorf("http body read error: %w", err))
-		}
+		go fetch(url, ch)
 	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+}
+
+func fetch(url string, ch chan int) {
+	fmt.Printf("fetch start: %s\n", time.Now().Format("15:04:05.000"))
+	if !strings.HasPrefix(url, "http") {
+		url = fmt.Sprintf("%s%s", "http://", url)
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(fmt.Errorf("http get error: %w", err))
+	}
+	ch <- res.StatusCode
+	//_, err = io.Copy(os.Stdout, res.Body)
+	//if err := res.Body.Close(); err != nil {
+	//	log.Println(fmt.Errorf("http body close error: %w", err))
+	//}
+	fmt.Printf("fetch finish: %s\n", time.Now().Format("15:04:05.000"))
 }
