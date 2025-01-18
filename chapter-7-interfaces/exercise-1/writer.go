0786606ead724e65855d7c3495543ec972ad6262
lapeko <lapeko88@gmail.com>
2025-01-18 15:07:35 +0100

    implement writer interface task

---
+++ chapter-7-interfaces/exercise-1/writer.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	logFolderName     = "logs"
+	logPrefix         = "log"
+	logSuffix         = ".txt"
+	logFileSize   int = 40
+)
+
+var (
+	once sync.Once
+	mut  sync.Mutex
+)
+
+type fileLogWriter struct {
+	writer            io.Writer
+	currentLogFileNum int
+}
+
+func (flw *fileLogWriter) Write(p []byte) (n int, err error) {
+	timeLogPrefix := time.Now().Format("[02.01.06 15:04:05] ")
+	buf := bytes.NewBufferString(timeLogPrefix)
+	buf.Write(p)
+	n, err = flw.writer.Write(buf.Bytes())
+
+	mut.Lock()
+	defer mut.Unlock()
+
+	writtenBytes, bytesToWrite := 0, len(p)
+	var file *os.File
+	for writtenBytes < len(p) {
+		var e error
+		file, e = openForAppend(flw.currentLogFileNum)
+		if e != nil {
+			log.Printf("open file failure%v\n", err)
+			return
+		}
+		stat, e := file.Stat()
+		if e != nil {
+			log.Printf("file stat read failure%v\n", err)
+			file.Close()
+			return
+		}
+
+		freeFileSpace := logFileSize - int(stat.Size())
+		nextChunkSize := minNum(freeFileSpace, bytesToWrite)
+
+		if freeFileSpace == nextChunkSize {
+			flw.currentLogFileNum++
+		}
+
+		_, e = file.Write(p[writtenBytes : writtenBytes+nextChunkSize])
+		file.Close()
+
+		writtenBytes += nextChunkSize
+		bytesToWrite -= nextChunkSize
+	}
+
+	return
+}
+
+func NewFileLogWriter(writer io.Writer) io.Writer {
+	flw := &fileLogWriter{
+		writer:            writer,
+		currentLogFileNum: 1,
+	}
+
+	once.Do(func() {
+		files, err := os.ReadDir(filepath.Join(logFolderName))
+
+		if err != nil {
+			if err := os.Mkdir(filepath.Join(logFolderName), 0755); err != nil {
+				panic(fmt.Sprintf("Failed to create logs folder: %v", err))
+			}
+		}
+
+		var nameNums []int
+		for _, f := range files {
+			if f.IsDir() {
+				continue
+			}
+			name := f.Name()
+			if !strings.HasPrefix(name, logPrefix) || !strings.HasSuffix(name, logSuffix) {
+				continue
+			}
+			strNum := strings.TrimSuffix(strings.TrimPrefix(name, logPrefix), logSuffix)
+			num, err := strconv.Atoi(strNum)
+			if err != nil {
+				log.Printf("log file parse error: %v\n", err)
+				continue
+			}
+			nameNums = append(nameNums, num)
+		}
+
+		if len(nameNums) == 0 {
+			return
+		}
+
+		slices.Sort(nameNums)
+		flw.currentLogFileNum = nameNums[len(nameNums)-1]
+	})
+
+	return flw
+}
+
+func getLogFilePath(fileNum int) string {
+	fileName := fmt.Sprintf("%s%d%s", logPrefix, fileNum, logSuffix)
+	return filepath.Join(logFolderName, fileName)
+}
+
+type num interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func minNum[T num](a T, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+func maxNum[T num](a T, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func openForAppend(logFileNum int) (*os.File, error) {
+	file, err := os.OpenFile(getLogFilePath(logFileNum), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+func main() {
+	withFileLogger := NewFileLogWriter(os.Stdout)
+	_, _ = withFileLogger.Write([]byte("Here are we go. This is a long log and should be written into several files and also logs into Stdout\n"))
+}
