5b7593f003985c6c74a9caa82a49e187e977e41b
lapeko <lapeko88@gmail.com>
2025-01-19 17:32:01 +0100

    use in/out channels in arguments

--- chapter-8-goroutines/exercise-4-channels/simple-pipeline.go
+++ chapter-8-goroutines/exercise-4-channels/simple-pipeline.go
@@ -6,25 +6,49 @@ func main() {
 	natural := make(chan int)
 	squares := make(chan int)
 
-	go func() {
-		for i := 0; i < 10; i++ {
-			natural <- i
-		}
-		close(natural)
-	}()
+	go genNaturals(natural)
+	go squareNaturals(natural, squares)
+	printSquares(squares)
 
-	go func() {
-		for {
-			nat, ok := <-natural
-			if !ok {
-				break
-			}
-			squares <- nat * nat
-		}
-		close(squares)
-	}()
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		natural <- i
+	//	}
+	//	close(natural)
+	//}()
+	//
+	//go func() {
+	//	for {
+	//		nat, ok := <-natural
+	//		if !ok {
+	//			break
+	//		}
+	//		squares <- nat * nat
+	//	}
+	//	close(squares)
+	//}()
+	//
+	//for square := range squares {
+	//	fmt.Println(square)
+	//}
+}
 
-	for square := range squares {
-		fmt.Println(square)
+func genNaturals(out chan<- int) {
+	for i := 0; i < 10; i++ {
+		out <- i
+	}
+	close(out)
+}
+
+func squareNaturals(in <-chan int, out chan<- int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printSquares(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
 	}
 }
