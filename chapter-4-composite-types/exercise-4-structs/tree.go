29bad7080030f88f746bda4d645f9527bf395374
lapeko <lapeko88@gmail.com>
2025-01-24 10:34:52 +0100

    implement sort int tree

---
+++ chapter-4-composite-types/exercise-4-structs/tree.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+func (t *tree) add(value int) *tree {
+	target := &t.right
+	if value < t.value {
+		target = &t.left
+	}
+	if *target == nil {
+		*target = &tree{value: value}
+	} else {
+		(*target).add(value)
+	}
+	return t
+}
+
+func (t *tree) getValues() (values []int) {
+	if t.left != nil {
+		values = append(values, t.left.getValues()...)
+	}
+	values = append(values, t.value)
+	if t.right != nil {
+		values = append(values, t.right.getValues()...)
+	}
+
+	return values
+}
+
+func main() {
+	fmt.Println((&tree{value: 50}).
+		add(24).
+		add(75).
+		add(13).
+		add(78).
+		getValues())
+}
