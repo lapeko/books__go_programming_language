d739b3e2899b6ef4de2a0b3a2c5e488ed85997a3
lapeko <lapeko88@gmail.com>
2025-01-25 08:53:51 +0100

    apply optimised multithreadong

--- chapter-3-primitive-types/exercise-3-complex/mandelbrot.go
+++ chapter-3-primitive-types/exercise-3-complex/mandelbrot.go
@@ -1,27 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"math/cmplx"
 	"os"
+	"runtime"
+	"sync"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	defer func() {
+		fmt.Fprintln(os.Stderr, time.Since(start))
+	}()
+
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 1024, 1024
 	)
+
+	threadsNum := runtime.NumCPU()
+	wg := &sync.WaitGroup{}
+	heightCh := make(chan int)
+
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
-		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
-		}
+
+	for i := 0; i < threadsNum; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for h := range heightCh {
+				y := float64(h)/height*(ymax-ymin) + ymin
+				for px := 0; px < width; px++ {
+					x := float64(px)/width*(xmax-xmin) + xmin
+					z := complex(x, y)
+					img.Set(px, h, mandelbrot(z))
+				}
+			}
+		}()
 	}
+
+	for i := 0; i < height; i++ {
+		heightCh <- i
+	}
+	close(heightCh)
+
+	wg.Wait()
 	png.Encode(os.Stdout, img)
 }
 
