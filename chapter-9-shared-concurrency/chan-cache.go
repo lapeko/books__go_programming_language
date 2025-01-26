302963e62aed78318b8472c2ebe2da6493f24217
lapeko <lapeko88@gmail.com>
2025-01-26 20:57:53 +0100

    move file

--- chapter-9-shared-concurrency/chan-cache.go
+++ chapter-9-shared-concurrency/chan-cache.go
@@ -1,5 +1,9 @@
 package main
 
+//Exercise 9.4: Construct a pipeline that connects an arbitrary number of goroutines with chan-
+//nels. What is the maximum number of pipeline stages you can create without running out of
+//memory? How long does a value take to transit the entire pipeline?
+
 type Func func(key string) (interface{}, error)
 
 type result struct {
